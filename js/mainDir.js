661f24664868592a2d37ab45b746510f02245987
Wang Bhotia <wangbhotia@gmail.com>
2017-04-09 17:55:29 -0600

    add DVD page

--- js/mainDir.js
+++ js/mainDir.js
@@ -1,21 +1,17 @@
 angular.module('devflix').directive('mainDir', function(){
 
 	return {
-		restrict: 'AE',
-		controller: 'mainCtrl',
+		restrict: 'A',
 
 		link: function(scope, element, attrs){
-
 			element.hover(function(){
 				element.transition({scale: 1.1});
 			},
 			function(){
 				element.transition({scale: 1});
 			});
-
-
 		}
+		
 	}
-
 
 })
